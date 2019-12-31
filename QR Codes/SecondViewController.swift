fbca6e88952bb005a98231f88f4fc85402587e82
Kyle Howells <freerunnering@gmail.com>
2019-12-31 22:14:55 +0000

    Initial Commit

---
+++ QR Codes/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  QR Codes
+//
+//  Created by Kyle Howells on 31/12/2019.
+//  Copyright © 2019 Kyle Howells. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
